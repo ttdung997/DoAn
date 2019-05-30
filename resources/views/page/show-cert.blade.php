a483e3f49f2b703d926551fbd921155f8c6f85bf
redhero1996 <frog.dbsk.cass@gmail.com>
2019-05-31 01:13:57 +0700

    edit display valid

--- resources/views/page/show-cert.blade.php
+++ resources/views/page/show-cert.blade.php
@@ -92,7 +92,7 @@
                     <div class="col-md-4 mt-3 ml-5">
                         <span><strong>Not Valid After:</strong></span>
                     </div>
-                    <div class="col-md-2 mt-3">
+                    <div class="col-md-3 mt-3">
                         <p>{{ $certificate->valid_to_time }}</p>
                     </div>
                     @if (strtotime($certificate->valid_to_time) < time())
